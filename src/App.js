b9235541f09a7a566a97dd972cec36b5b9ba5fad
anjalsaneen <anjalsaneen123@gmail.com>
2018-09-03 17:37:47 +0530

    added api

--- src/App.js
+++ src/App.js
@@ -5,6 +5,26 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  componentWillMount() {
+    const obj = {  
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Origin': '',
+        'Host': 'api.producthunt.com',
+        'Access-Control-Allow-Origin': '*'
+      },
+    }
+    fetch('https://fantasy.premierleague.com/a/team/308784/event/4', obj)  
+    .then(function(res) {
+      return res.json();
+    })
+    .then(function(resJson) {
+      console.log(resJson);
+    })
+  }
+
   render() {
     return (
       <div className="App">
